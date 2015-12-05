b4af1609d6c204abd921b19b7531d0968c877703
Adam Stokes <adam.stokes@ubuntu.com>
2015-12-05 18:39:31 -0500

    update xo
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- api.js
+++ api.js
@@ -1,61 +1,61 @@
 "use strict";
 
-let Hapi = require("hapi");
-let Config = require("getconfig");
-let Good = require("good");
-let Minimist = require("minimist");
-let api = require("./api");
-let authPlugin = require("./api/authentication");
-let serverConfig = {};
-let server = new Hapi.Server(serverConfig);
+const Hapi = require("hapi");
+const Config = require("getconfig");
+const Good = require("good");
+const Minimist = require("minimist");
+const api = require("./api");
+const authPlugin = require("./api/authentication");
+const serverConfig = {};
+const server = new Hapi.Server(serverConfig);
 
 let argv = Minimist(process.argv.slice(2), {
-  boolean: ['help'],
-  default: {
-    host: 'localhost',
-    port: 3001
-  }
+	boolean: ['help'],
+	default: {
+		host: 'localhost',
+		port: 3001
+	}
 });
 
 if (argv.help) {
-  console.log('node index.js --host=localhost --port=3001');
-  process.exit(0);
+	console.log('node index.js --host=localhost --port=3001');
+	process.exit(0);
 }
 
 server.connection({
-  host: Config.api.host,
-  port: Config.api.port
+	host: Config.api.host,
+	port: Config.api.port
 });
 
 let plugins = [];
 plugins.push({
-  register: Good,
-  options: {
-    reporters: [{
-      reporter: require("good-console"),
-      events: {
-        // Move to good-file
-        response: "*",
-        log: "*"
-      }
-    }]
-  }
+	register: Good,
+	options: {
+		reporters: [{
+			reporter: require("good-console"),
+			events: {
+				// Move to good-file
+				response: "*",
+				log: "*"
+			}
+		}]
+	}
 });
 
 plugins.push({
-  register: authPlugin
+	register: authPlugin
 });
 
 plugins.push({
-  register: api
+	register: api
 });
 
 server.register(plugins, err => {
-  if (err) {
-    throw err;
-  }
+	if (err) {
+		throw err;
+	}
 });
 
 server.start(() => {
-  console.info(`Server running at: ${server.info.uri}`);
+	console.info(`Server running at: ${server.info.uri}`);
 });
